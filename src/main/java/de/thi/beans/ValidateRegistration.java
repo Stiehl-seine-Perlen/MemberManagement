1856e07b3d0b3e3525ecb7d33d24396018accbf3
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2023-04-10 17:36:50 +0200

    Process execution works (validation logic is missing)

--- src/main/java/de/thi/beans/ValidateRegistration.java
+++ src/main/java/de/thi/beans/ValidateRegistration.java
@@ -1,12 +1,10 @@
 package de.thi.beans;
 
 import de.thi.entities.User;
-import io.quarkus.logging.Log;
-import org.kie.kogito.internal.process.runtime.KogitoProcessContext;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.core.Response;
+import javax.transaction.Transactional;
 
 @ApplicationScoped
 // Get process context of kogito application
@@ -29,53 +27,55 @@ public class ValidateRegistration {
     //Get Process Context from Kogito Application and validate username
     //TODO: Refine username validation
     public boolean validateUsername(User user) {
-
-
-        User searchedUser = userRegistration.find("username", user.getUsername()).firstResult();
-
-        //test if username is already in database
-        if(searchedUser != null){
-            //log Entry: Username already exists
-            Log.info("Username >>"+ user.getUsername()+"<< already exists");
-            return false;
-        }
-        //Username is not already in database -> Vald new username
-        Log.info("Username >>"+ user.getUsername()+"<< not found -> valid new username");
+//
+//
+//        User searchedUser = userRegistration.find("username", user.getUsername()).firstResult();
+//
+//        //test if username is already in database
+//        if(searchedUser != null){
+//            //log Entry: Username already exists
+//            Log.info("Username >>"+ user.getUsername()+"<< already exists");
+//            return false;
+//        }
+//        //Username is not already in database -> Vald new username
+//        Log.info("Username >>"+ user.getUsername()+"<< not found -> valid new username");
         return true;
     }
     public boolean validatePassword(User user){
-
-        String password = user.getPassword();
-        if(password != null && password.length() >= 8){
-            return true;
-        }
-        else {
-            return false;
-        }
+        return true;
+//        String password = user.getPassword();
+//        if(password != null && password.length() >= 8){
+//            return true;
+//        }
+//        else {
+//            return false;
+//        }
     }
     private boolean validateEmail(User user) {
 
-        String email = user.getEmail();
-        User searchedUser = userRegistration.find("email", user.getEmail()).firstResult();
-
-        //Validate email with regex
-
-        //Regex from ChatGPT
-        String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
-
-        if(searchedUser == null && email.matches(regex)){
-            return true;
-        }
-        else {
-            return false;
-        }
+//        String email = user.getEmail();
+//        User searchedUser = userRegistration.find("email", user.getEmail()).firstResult();
+//
+//        //Validate email with regex
+//
+//        //Regex from ChatGPT
+//        String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+//
+//        if(searchedUser == null && email.matches(regex)){
+//            return true;
+//        }
+//        else {
+//            return false;
+//        }
+        return true;
     }
 
-    private Response promptfornewInput(){
-        return Response.status(422).build();
+    public void promptfornewInput(){
+        System.out.println("Prombt for new input");
     }
 
-    private void registerUser(User user){
+    @Transactional
+    public void registerUser(User user){
         userRegistration.persist(user);
     }
 }
--- src/main/java/de/thi/rest/UserRegistrationEndpoint.java
+++ src/main/java/de/thi/rest/UserRegistrationEndpoint.java
@@ -3,7 +3,6 @@ package de.thi.rest;
 import de.thi.beans.UserRegistration;
 import de.thi.entities.User;
 import org.json.JSONObject;
-import org.kie.api.runtime.process.ProcessContext;
 
 
 import javax.inject.Inject;
@@ -18,7 +17,6 @@ import javax.ws.rs.core.Response;
 @Path("/user")
 public class UserRegistrationEndpoint {
 
-    @Inject ProcessContext processContext;
     @Inject
     UserRegistration userRegistration;
 
@@ -33,7 +31,6 @@ public class UserRegistrationEndpoint {
         JSONObject userJson = new JSONObject(user);
         userJson.remove("password");
         //write user into process context
-        processContext.setVariable("user", user);
         return Response.status(200).entity(userJson.toString()).build();
     }
 
