a06cc6992ab21621ad2ed756cf3d475d7d0ce291
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2023-04-30 19:51:01 +0200

    ➕added quarkus-mailer

--- src/main/java/de/thi/servicetask/NewUserMessages.java
+++ src/main/java/de/thi/servicetask/NewUserMessages.java
@@ -1,18 +1,40 @@
 package de.thi.servicetask;
 
+import io.quarkus.mailer.reactive.ReactiveMailer;
+import io.smallrye.mutiny.Uni;
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class NewUserMessages {
 
 
     public void sendWelcomeEmail(String userID) {
-        System.out.println("Welcome Email sent to .... "+ userID);
-
+        System.out.println("Welcome Email sent to .... " + userID);
+        sendEmail(userID);
     }
 
     public void sendRecommendationEmail(String userID) {
-        System.out.println("Recommendation Email sent to .... "+ userID);
+        System.out.println("Recommendation Email sent to .... " + userID);
+        sendEmail(userID);
 
     }
+
+    //TODO: Make maler async (reactive Mailer)
+    @Inject
+    Mailer mailer;
+
+
+    public void sendEmail(String userID) {
+        mailer.send(
+                Mail.withText("no-reply@benevolo.de",
+                    "Ahoy from Quarkus",
+                    "Hello to User: "+ userID
+                )
+        );
+    }
+
+
 }
