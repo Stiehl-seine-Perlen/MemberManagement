fcb2cf8dcd6785feb5bee983bc399dac11dcd6da
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2023-04-30 19:14:42 +0200

    🚧fixed var for called Process NewUserMessageProcess.bpmn

--- src/main/java/de/thi/servicetask/NewUserMessages.java
+++ src/main/java/de/thi/servicetask/NewUserMessages.java
@@ -5,7 +5,6 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class NewUserMessages {
 
-    //TODO: investigate why userID is printed instead of the id
 
     public void sendWelcomeEmail(String userID) {
         System.out.println("Welcome Email sent to .... "+ userID);
