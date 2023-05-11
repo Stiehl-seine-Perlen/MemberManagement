78818407fb94a2185541f9c86ac0897dd9a093ec
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2023-05-11 16:41:47 +0200

    ✨PlatformUserRepository.java

---
+++ src/main/java/de/thi/repositories/PlatformUserRepository.java
@@ -0,0 +1,10 @@
+package de.thi.repositories;
+
+import de.benevolo.entities.user.PlatformUser;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+import java.util.List;
+
+public class PlatformUserRepository implements PanacheRepository<PlatformUser> {
+    //auto implemented
+}
