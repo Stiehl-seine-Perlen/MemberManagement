27706270102e0e23e334cc231d49c22e63a71f04
Philip Lobmeier <phl3077@thi.de>
2023-05-11 11:13:02 +0200

    :sparkles: Implementation done

--- src/main/java/de/thi/CheckMembership.java
+++ src/main/java/de/thi/CheckMembership.java
@@ -25,31 +25,29 @@ public class CheckMembership {
 
     @Transactional
     public boolean checkStatus(PlatformUser user, Association association) {
-
-        // Tests - Ignore it
-        if (user != null && association != null) {
-            LOGGER.info("User passed: " + user.getName() + "\tAssociation passed: " + association.getAssociationName());
-        } else {
-            LOGGER.info("LEER: -------------------------------------- ");
-        }
+        
+        boolean result = false;
 
         try {
-            LOGGER.info("Getting all Memberships with specific UserId.");
-            List<Membership> userMembershipList = membershipRepository.list("userid", user.getId());            
+             
+            LOGGER.info("Getting all Memberships with User -> " + user.getName() + " and Association -> " + association.getAssociationName());
+
+            // Code to select Membership with matching association id und user id
+            // When no matches list is empty
+
+            List<Membership> userMembershipList = membershipRepository.list("FROM Membership WHERE associationrole_associationroleid IS " + association.getId() + " AND userid IS " + user.getId());
             LOGGER.info("Generated List: " + userMembershipList);
 
-            // <--- Insert Code to select Membership with matching Association --->
-
-            // Idea: Predict Membership from passed user and association and compare it with list items
-            Membership predictedMembership = new Membership(user.getId(), null, association);
-
-            return true; // -> Already Member
-            
+            if(userMembershipList.isEmpty()){
+                result = false; // -> No Member
+            }else{
+                result = true; // -> Already Member
+            }
+           
         } catch (Exception e) {
-            LOGGER.error("Exception -> Error : ", e);
-            return false; // -> No Member
-        }
+            LOGGER.error("Exception -> Error: Something wrong in fetching Data from Memberships");
+        }; 
 
+        return result;
     }
-
 }
