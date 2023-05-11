1dc1a308ef62611883fe38f8ec29f87c76e09ac5
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2023-05-11 16:45:24 +0200

    🚨@ApplicationScoped

--- src/main/java/de/thi/repositories/PlatformUserRepository.java
+++ src/main/java/de/thi/repositories/PlatformUserRepository.java
@@ -3,8 +3,9 @@ package de.thi.repositories;
 import de.benevolo.entities.user.PlatformUser;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
-import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class PlatformUserRepository implements PanacheRepository<PlatformUser> {
     //auto implemented
 }
