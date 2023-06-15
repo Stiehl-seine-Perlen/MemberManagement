1abf9fa07385776c29b5f8790921ce99287b7339
Matthias Raimann <matthias.raimann82@gmail.com>
2023-06-15 16:38:23 +0200

    mocked the user service

--- src/main/java/de/thi/services/PlatformUserService.java
+++ src/main/java/de/thi/services/PlatformUserService.java
@@ -1,5 +1,7 @@
 package de.thi.services;
 
+import com.github.javafaker.Name;
+import de.benevolo.entities.global.Address;
 import de.benevolo.entities.user.PlatformUser;
 import de.thi.repositories.PlatformUserRepository;
 
@@ -8,6 +10,9 @@ import javax.inject.Inject;
 import javax.transaction.Transactional;
 import java.util.List;
 
+import com.github.javafaker.Faker;
+import org.checkerframework.checker.units.qual.A;
+
 @ApplicationScoped
 public class PlatformUserService {
 
@@ -15,7 +20,28 @@ public class PlatformUserService {
     PlatformUserRepository platformUserRepository;
 
     public PlatformUser getPlatformUserById(Long id){
-        return platformUserRepository.findById(id);
+        PlatformUser platformUser = platformUserRepository.findById(id);
+        //TODO: remove this mock later on
+        if(platformUser == null) {
+            Faker faker = new Faker();
+            Name name = faker.name();
+            String firstName = name.firstName();
+            String lastName = name.lastName();
+            platformUser = new PlatformUser(
+                    firstName + " " + lastName,
+                    firstName.toLowerCase() + "." + lastName.toLowerCase() + "@mock.de"
+            );
+
+
+            platformUser.setAddress(new Address(
+                    faker.address().streetName(),
+                    faker.address().buildingNumber(),
+                    faker.address().zipCode(),
+                    faker.address().city(),
+                    faker.address().country()
+            ));
+        }
+        return platformUser;
     }
 
     public List<PlatformUser> getAllPlatformUsers(){
