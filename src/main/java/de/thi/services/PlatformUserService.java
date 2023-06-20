31da1c8219f1f85d14e52056b04cc8866c8f6e2d
Hubertus Seitz <81434615+Ainges@users.noreply.github.com>
2023-06-20 17:51:21 +0200

    🔥removed kogito dependencies

--- src/main/java/de/thi/services/PlatformUserService.java
+++ src/main/java/de/thi/services/PlatformUserService.java
@@ -11,7 +11,6 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 import com.github.javafaker.Faker;
-import org.checkerframework.checker.units.qual.A;
 
 @ApplicationScoped
 public class PlatformUserService {
